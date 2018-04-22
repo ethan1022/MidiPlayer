a7ca4dff4d1ba964237b9a200d654df6b8ffe0b4
ethan1022 <ethan1022@gmail.com>
2018-04-22 15:18:09 +0800

    first commit

---
+++ MidiPlayer/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MidiPlayer
+//
+//  Created by Ethan on 2018/4/21.
+//  Copyright © 2018年 EthanLab. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
