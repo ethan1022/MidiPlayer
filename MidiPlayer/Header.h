50f432fdd59f6dd3d91ec9482ece8d0a9cccf3d9
ethan1022 <ethan1022@gmail.com>
2018-04-25 12:50:13 +0800

    deal with conflict

---
+++ MidiPlayer/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  MidiPlayer
+//
+//  Created by Ethan on 2018/4/20.
+//  Copyright © 2018年 EthanLab. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
